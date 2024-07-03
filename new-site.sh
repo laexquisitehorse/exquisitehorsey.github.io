8cbd45f0fb6a24c4ff631b872afead5479d49908
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-07-03 05:26:12 +0000

    quick-update

--- new-site.sh
+++ new-site.sh
@@ -1,4 +1,1 @@
-echo "What is the new username?"
-read URL
-
-git remote set-url origin https://$URL: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@github.com/$URL/$URL.github.io.git
+git remote set-url origin https://unblockedgames4life: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@github.com/unblockedgames4life/unblockedgames4life.github.io.git
