017a471e9fa9666dbf8cd9348a354cff74540701
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-07-03 04:12:13 +0000

    mini-update

---
+++ edit.sh
@@ -0,0 +1,33 @@
+echo "Saving..."
+
+git add .
+git commit -m "mini-update"
+git pull origin main
+clear
+
+echo "              -"
+sleep 0.5
+clear
+
+echo "              --"
+sleep 0.5
+clear
+
+echo "              ---"
+sleep 0.5
+clear
+
+echo "              -"
+sleep 0.5
+clear
+
+echo "              --"
+sleep 0.5
+clear
+
+echo "              ---"
+sleep 0.7
+clear
+
+echo "Done!"
+sleep 1
