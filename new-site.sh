fc1f648a9736e44e82c45624ef1f446afc7fbb2b
shragsir <32632850-shragsir@users.noreply.replit.com>
2024-07-03 05:18:25 +0000

    quick-update

--- new-site.sh
+++ new-site.sh
@@ -1,4 +1,4 @@
 echo "What is the new username?"
 read URL
 
-git remote set-url origin https://$URL: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@github.com/$URL/$URL.github.io.git
+git remote set-url origin https://$URL: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@github.com/$URL/$URL.github.io.git
