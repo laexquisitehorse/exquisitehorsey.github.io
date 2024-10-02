93e92c736f55f0ccecad93f43dd0949f8dddcbec
horsefacts4u <162401234+horsefacts4u@users.noreply.github.com>
2024-10-02 15:59:39 -0800

    Update script.js

--- testgamepage/tewr/script.js
+++ testgamepage/tewr/script.js
@@ -40,3 +40,102 @@ function fadeAffect() {
         requestAnimationFrame(fadeOut);
     }
 }
+
+function weather() {
+    /* Run this every 1o minutes: */
+    var weatherID = math.floor(math.random() * argument.length);
+    return arguments[weatherID];
+
+    if ('weatherID' === 1 || 'weatherID' === 2 || 'weatherID' === 3 || 'weatherID' === 4) {
+        var weatherName = 'sunny';
+        console.log('server: weather=sunny');
+    } else if ('weatherID' === 5 || 'wetaherID' === 6) {
+        var weatherName = 'cloudy';
+        console.log('server: weather=cloudy');
+    } else if ('weatherID' === 7 || 'weatherID' === '8' || 'weatherID' === '9') {
+        var weatherName = 'rainy';
+        console.log('server: weather=rainy');
+    } else if ('wetherID' === '10') {
+        var weatherName = 'stormy';
+        console.log('server: weather=stormy');
+    }
+    /* Put the actual weather that is visible here */
+}
+
+function tradeChances() {
+    var tradeItemVar1 = Math.floor(Math.random() * 34) + 1;
+    return arguments[tradeItemVar1];
+    var tradeItemVar2 = Math.floor(Math.random() * 34) + 1;
+    return arguments[tradeItemVar2];
+    var tradeItemVar3 = Math.floor(Math.random() * 34) + 1;
+    return arguments[tradeItemVar3];
+    var tradeItemVar4 = Math.floor(Math.random() * 34) + 1;
+    return arguments[tradeItemVar4];
+    var tradeItemVar5 = Math.floor(Math.random() * 35);
+    return arguments[tradeItemVar5];
+    var tradeItemVar6 = Math.floor(Math.random() * 35);
+    return arguments[tradeItemVar6];
+}
+
+function loadTrade() {
+    tradeChances()
+
+    if ('tradeItemVar5' === 0) {
+        var tradeItemVar6 = 0;
+        return arguments[tradeItemVar6];
+        displayTradeItems()
+    } else {
+        displayTradeItems()
+    }
+}
+
+function displayTradeItems() {
+    if ('tradeItemVar1' === 1) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 2) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 3) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 4) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 5) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 6) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 7) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 8) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 9) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 10) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 11) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 12) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 13) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 14) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    } else if ('tradeItemVar1' === 15) {
+        var tradeItemName1 = '';
+        return arguments[tradeItemName1];
+    }
+}
+
+
