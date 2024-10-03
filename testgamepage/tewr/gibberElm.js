8b600bb7341eaaac7b8c7bff86a74c45efae0235
horsefacts4u <162401234+horsefacts4u@users.noreply.github.com>
2024-10-03 12:00:28 -0800

    Update gibberElm.js

--- testgamepage/tewr/gibberElm.js
+++ testgamepage/tewr/gibberElm.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'w' || event.key === 'W') {
+    alert('key pressed: W')
+  } else if (event.key === 'a' || event.key === 'A') {
+    alert('key pressed: A')
+  }
+}
