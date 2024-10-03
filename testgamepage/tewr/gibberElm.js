7351d5f09b52e358b0f66ec5daca85d0747dc4be
horsefacts4u <162401234+horsefacts4u@users.noreply.github.com>
2024-10-03 15:21:20 -0800

    Update gibberElm.js

--- testgamepage/tewr/gibberElm.js
+++ testgamepage/tewr/gibberElm.js
@@ -1,7 +1,23 @@
 document.addEventListener('keydown', function(event) {
   if (event.key === 'w' || event.key === 'W') {
-    alert('key pressed: W')
+    alert('key pressed: W');
   } else if (event.key === 'a' || event.key === 'A') {
-    alert('key pressed: A')
+    alert('key pressed: A');
+  } else if (event.key === 's' || event.key === 'S') {
+    alert('key pressed: S');
+  } else if (event.key === 'd' || event.key === 'D') {
+    alert('key pressed: D');
+  }
+};
+
+document.addEventListener('keyup', function(event) {
+  if (event.key === 'w' || event.key === 'W') {
+    alert('key unpressed: W');
+  } else if (event.key === 'a' || event.key === 'A') {
+    alert('key unpressed: A');
+  } else if (event.key === 's' || event.key === 'S') {
+    alert('key unpressed: S');
+  } else if (event.key === 'd' || event.key === 'D') {
+    alert('key unpressed: D');
   }
 }
